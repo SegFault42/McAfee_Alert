95bbb90f37a2671c8a4704f69830a40b6a40508b
SegFault42 <SegFault42@protonmail.com>
2017-12-24 06:30:58 +0100

    Check only after new tweet

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 import sys
+import sys
+import time
 
 consumer_key = ""
 consumer_secret = ""
@@ -21,9 +23,9 @@ api = tweepy.API(auth)
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("YOUR_MAIL", "YOUR_PASS")
-fromAddr = "YOUR_MAIL"
-toAddr = "TARGET_MAIL"
+server.login("ramzi90000@gmail.com", "")
+fromAddr = "ramzi90000@gmail.com"
+toAddr = "SegFault42@protonmail.com"
 
 def tweet_current_value(tweet):
     try:
@@ -45,15 +47,24 @@ def sendMail(tweet):
     server.sendmail(fromAddr, toAddr, text)
     server.quit()
 
+def getCryptoOftheDay(tweet):
+    tweet = tweet.split(' ')
+    return tweet[4]
+
 def alert(tweet):
-    sendMail(tweet)
+    #sendMail(tweet)
+    print getCryptoOftheDay(tweet)
 
 def main():
-    #tweet_current_value("Hello world")
-    stuff = api.user_timeline(screen_name = '@officialmcafee', count = 10, include_rts = True)
-    for status in stuff:
-        if "Coin of the day" in status.text:
-            alert(status.text);
+    lastTweet = ""
+    while True:
+        stuff = api.user_timeline(screen_name = '@officialmcafee', count = 1, include_rts = False)
+        for status in stuff:
+            if lastTweet != status.text :
+                lastTweet = status.text
+                if "Coin of the day" in status.text:
+                    alert(status.text);
+        time.sleep(1)
 
 if __name__ == '__main__':
     main()
