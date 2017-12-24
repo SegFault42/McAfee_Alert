052bffb02c15c444f2199f45ecd042631b3316e9
rmc-coma <rmc-coma@student.42.fr>
2017-12-24 14:23:37 +0100

    fuck fuck fuck

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ def allIn(token, market, btc_balance):
     if buy['success'] != True:
         logger("FATAL ERROR : " + buy['message'])
         return (False)
-    for key in buy['result'].keys()
+    for key in buy['result'].keys():
         logger(str(key) + ": " + str(buy['result'][key]))
     return (True)
     
